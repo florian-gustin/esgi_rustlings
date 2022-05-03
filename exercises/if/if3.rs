69adf06fe163f8f24c3c63390e31b2506aba4560
Florian Gustin <florian.gustin.business@gmail.com>
2022-05-03 09:48:07 +0200

    init

--- exercises/if/if3.rs
+++ exercises/if/if3.rs
@@ -3,7 +3,6 @@
 // Step 2: Get the tests passing!
 // Execute `rustlings hint if3` for hints!
 
-// I AM NOT DONE
 
 fn compute_fees(price: i32) -> i32 {
     let fees: i32; // (mut not allowed)
@@ -11,7 +10,9 @@ fn compute_fees(price: i32) -> i32 {
     // * fees for price price higher than 10 is 10% rounded down (use / 10)
     // don't change before that line
     if price < 10 {
-        fees = 1;
+        fees = 1
+    } else{
+        fees = price / 10
     }
     // don't change after that line
     fees
